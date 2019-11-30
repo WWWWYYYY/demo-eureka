d88f3cbba9df11604551edd2d5aa74ae54250443
wangyi <394489664@qq.com>
2019-11-30 11:24:56 +0800

    *init

---
+++ src/main/java/com/example/demoeureka/common/Constants.java
@@ -0,0 +1,33 @@
+package com.example.demoeureka.common;
+
+/**
+ * Application constants.
+ */
+public final class Constants {
+
+    //Regex for acceptable logins
+    public static final String LOGIN_REGEX = "^[_'.@A-Za-z0-9-]*$";
+    // Spring profile for development and production, see http://jhipster.github.io/profiles/
+    public static final String SPRING_PROFILE_DEVELOPMENT = "dev";
+    public static final String SPRING_PROFILE_PRODUCTION = "prod";
+    public static final String SPRING_PROFILE_TEST = "test";
+    public static final String SPRING_PROFILE_AUTO_TEST = "autotest";
+    // Spring profile used when deploying with Spring Cloud (used when deploying to CloudFoundry)
+    public static final String SPRING_PROFILE_CLOUD = "cloud";
+    // Spring profile used when deploying to Heroku
+    public static final String SPRING_PROFILE_HEROKU = "heroku";
+    // Spring profile used to disable swagger
+    public static final String SPRING_PROFILE_SWAGGER = "swagger";
+    // Spring profile used to disable running liquibase
+    public static final String SPRING_PROFILE_NO_LIQUIBASE = "no-liquibase";
+
+    public static final String SYSTEM_ACCOUNT = "system";
+    // 启用定时器任务的profile name
+    public static final String SPRING_PROFILE_SCHEDULE = "schedule";
+
+    // 表单行状态
+    public static final Integer STATE_DELETE = 1;
+
+    private Constants() {
+    }
+}
